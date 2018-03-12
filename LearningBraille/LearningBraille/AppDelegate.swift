87c607648f8f0c69cdeb0fe137464284e4eac6a5
George Gomes <georgegomees@gmail.com>
2018-03-11 23:02:43 -0300

    First stage of the braille encoding algorithm (only uppercase or lowercase sensitivity)

--- LearningBraille/LearningBraille/AppDelegate.swift
+++ LearningBraille/LearningBraille/AppDelegate.swift
@@ -16,7 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        var o = BrailleConverter()
+        var b = o.translateThis("CA BA")
+        
+        for a in b{
+            for c in a{
+                print(c)
+            }
+        }
+        
         return true
     }
 

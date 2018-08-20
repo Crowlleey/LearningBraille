3f9cf7454cba872017b73e447792a34ea8b5a67b
George Gomes <georgegomees@gmail.com>
2018-08-20 20:46:57 -0300

    Created Login View Controller

---
+++ LearningBraille/LearningBraille/Controller/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  LearningBraille
+//
+//  Created by George Gomes on 20/08/18.
+//  Copyright © 2018 George Gomes. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController{
+    
+}

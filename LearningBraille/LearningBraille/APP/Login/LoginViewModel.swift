a704b94abb6bc55d9e7f80c5e0e3c0715ac2342c
George Gomes <georgegomees@gmail.com>
2018-08-23 22:07:19 -0300

    Created LoginViewModel file

---
+++ LearningBraille/LearningBraille/APP/Login/LoginViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewModel.swift
+//  LearningBraille
+//
+//  Created by George Gomes on 23/08/18.
+//  Copyright © 2018 George Gomes. All rights reserved.
+//
+
+import Foundation
+
+struct LoginViewModel {
+    
+}
